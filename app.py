5c6d7b48511acafd1c56b7516ffd263431a0766c
Vicky Kumar <imvickykumar999@gmail.com>
2022-07-13 17:09:59 +0530

    404

--- app.py
+++ app.py
@@ -26,7 +26,14 @@ def static_json():
 	num = random.randint(1, 10)
 	return jsonify({"image":f"https://randomfrog.herokuapp.com/static/{num}.jpg","link":f"https://randomfrog.herokuapp.com/?i={num}"})
 
+
 # https://stackoverflow.com/a/57910276/11493297
+
+@app.errorhandler(404)
+def page_not_found(e):
+    e = 'Page not Found'
+    return ( render_template('404.html', e=e), 404 )
+
 
 if __name__ == '__main__':
 	app.run(debug = True)
